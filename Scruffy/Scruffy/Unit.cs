d39a7c5f3d5ce2755fd79bbfb3cf1f5af11c56dc
Hampy <joshua-hampton@live.com>
2013-07-10 21:46:46 -0500

    added abstract member functions

--- Scruffy/Scruffy/Unit.cs
+++ Scruffy/Scruffy/Unit.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.Xna.Framework;
+
 //Class to manage a squad. Should be comprised of a list of models.
 namespace Scruffy
 {
     class Squad : DrawObject
     {
+        public override void Update(Vector2 mPos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
