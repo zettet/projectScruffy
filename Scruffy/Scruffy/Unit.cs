1145fd7b1e14611cfef1cc2b8d93e9afb1b0292d
Hans Vorsteveld <hvorsteveld@gmail.com>
2013-07-10 22:44:27 -0400

    Provide blank Update() functions so code compiles.

--- Scruffy/Scruffy/Unit.cs
+++ Scruffy/Scruffy/Unit.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.Xna.Framework;
+
 //Class to manage a squad. Should be comprised of a list of models.
 namespace Scruffy
 {
     class Squad : DrawObject
     {
+        public override void Update()
+        {
+        }
+
+        public override void Update(Vector2 pos)
+        {
+        }
     }
 }
